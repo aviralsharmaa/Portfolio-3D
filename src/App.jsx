745881e7b959c8c17844a4ea18341fa54157ea72
Aviral_Sharma <aviral2020it015@abesit.edu.in>
2023-03-15 13:36:59 +0530

    complete_Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-const App = () => {
+import { BrowserRouter } from 'react-router-dom';
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const App = () => {
   return (
-    <div>
-      3D developer portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className='bg-hero-pattern bf-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
-
 export default App
