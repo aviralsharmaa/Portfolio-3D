fd63ed344f0ad17204b6e2e0ac34f5e028a7763a
Aviral_Sharma <aviral2020it015@abesit.edu.in>
2023-03-18 18:45:16 +0530

    finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,10 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => {
-  return (
+const ServiceCard = ({ index, title, icon }) => (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
@@ -31,7 +30,6 @@ const ServiceCard = ({ index, title, icon }) => {
       </motion.div>
     </Tilt>
   );
-};
 
 const About = () => {
   return (
@@ -41,7 +39,7 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.div
+      <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] 
       max-w-3xl leading-[30px]"
@@ -55,12 +53,12 @@ const About = () => {
         of a team, gaining hands-on experience in software development. I am
         eager to contribute my skills and knowledge to a dynamic team and begin
         my career as a software developer.
+        </motion.p>
         <div className="mt-20 flex flex-wrap gap-10">
           {services.map((service, index) => (
             <ServiceCard key={service.title} index={index} {...service} />
           ))}
         </div>
-      </motion.div>
     </>
   );
 };
